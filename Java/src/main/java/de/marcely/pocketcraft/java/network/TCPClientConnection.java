cb9edf6eda7a2054b897d62f26edcc39ed065ee8
MrEAlderson <Mylogan99@web.de>
2019-10-03 18:42:59 +0200

    Added timeout

--- Java/src/main/java/de/marcely/pocketcraft/java/network/TCPClientConnection.java
+++ Java/src/main/java/de/marcely/pocketcraft/java/network/TCPClientConnection.java
@@ -17,12 +17,16 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import lombok.Getter;
+import lombok.Setter;
 
 public class TCPClientConnection extends Connection {
 	
 	@Getter private final InetAddress address;
 	@Getter private final int port;
+	
+	@Getter @Setter private int timeout = 6;
 	
 	private Channel channel;
 	
@@ -43,9 +47,10 @@ public class TCPClientConnection extends Connection {
 		bootstrap.remoteAddress(this.address, this.port);
 		bootstrap.handler(new ChannelInitializer<SocketChannel>(){
 			protected void initChannel(SocketChannel ch) throws Exception {
-				ch.pipeline().addLast(new PacketDecoder());
-				ch.pipeline().addLast(new PacketEncoder());
-				ch.pipeline().addLast(new PacketHandler());
+				ch.pipeline().addLast("timeout_handler", new ReadTimeoutHandler(timeout));
+				ch.pipeline().addLast("packet_decoder", new PacketDecoder());
+				ch.pipeline().addLast("packet_encoder", new PacketEncoder());
+				ch.pipeline().addLast("packet_handler", new PacketHandler());
 			}
 		});
 		
@@ -108,6 +113,11 @@ public class TCPClientConnection extends Connection {
 		@Override
 		protected void channelRead0(ChannelHandlerContext ctx, Packet packet) throws Exception {
 			packetReadQueue.add(packet);
+		}
+		
+		@Override
+	    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+			cause.printStackTrace();
 		}
 	}
 }
---
+++ JavaTest/run.cmd
@@ -0,0 +1,3 @@
+@echo off
+java -jar target/JavaTest-0.0.1-SNAPSHOT-shaded.jar localhost 19132
+pause
